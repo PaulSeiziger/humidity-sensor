1b7d6536bc54cd8e3bf62bfc2023f11ac59cf468
PaulSeiziger <pavel@dremanovich.com>
2015-05-03 16:50:40 +0300

    update

--- GUI/server.js
+++ GUI/server.js
@@ -1,8 +1,11 @@
 var http = require('http');
 var express = require('express');
 var app = express();
+var fs = require('fs');
+var bodyParser = require('body-parser');
+ 
 
-var config = require(__dirname + '/config/config.js');
+var config = require(__dirname + '/config/server_config.js');
 
 var serialport = require("serialport");
 var SerialPort = serialport.SerialPort;
@@ -21,7 +24,11 @@ serialPort.on("open", function () {
 
 
 	serialPort.on('data', function(data) {
-		humidity_value = (data[0] << 8) | data[1];
+		//Нужно чтобы входящий пакет состоял из 2-х байт
+		console.log(data);
+		if(data.length == 2){
+			humidity_value = (data[0] << 8) | data[1];
+		}
 	});
 
 	serialPort.on('error', function(err) {
@@ -31,7 +38,12 @@ serialPort.on("open", function () {
 
 
 app.use(express.static(__dirname + '/public'));
+app.use( bodyParser.json() );
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
 
+//Запрос данных о влажности
 app.get('/humidity', function (req, res) {
 	var buffer = new Buffer(1);
 	buffer.writeUInt8(config.commands.get_humidity, 0);
@@ -40,6 +52,22 @@ app.get('/humidity', function (req, res) {
     res.end(humidity_value.toString());
 });
 
+//Загрузка текущей конифгурации интерфейса
+app.get('/get_config', function (req, res) {
+	fs.readFile(__dirname + '/config/gui_config.json', function (err, data) {
+		if (err) throw err;
+		
+		res.set('Content-Type', 'application/json');
+		res.end(data);
+	});
+});
+
+//Сохранение конфигурации
+app.post('/set_config', function (req, res) {
+	console.log(req.body);
+});
+
+//Показ главной страницы
 app.get('/', function (req, res) {
 	res.redirect('index.html')
 });
